146375543acbe30d4b50b111882dbb5a39a57b43
Vasily Guzov <vasily.guzov@gmail.com>
2021-10-12 09:54:57 +0300

    clean CRA, add env variables

---
+++ src/components/App/App.tsx
@@ -0,0 +1,9 @@
+export function App() {
+  return (
+    <div className="App">
+      <header>
+        <a href={process.env.REACT_APP_API_URL}>Rick & Morty Party</a>
+      </header>
+    </div>
+  );
+}
