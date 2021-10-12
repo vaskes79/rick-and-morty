3a346e673feaab504fff0efc8467b67737517e12
Vasily Guzov <vasily.guzov@gmail.com>
2021-10-12 19:57:25 +0300

    delete handler for character

--- src/components/CharacterItem/CharacterItem.tsx
+++ src/components/CharacterItem/CharacterItem.tsx
@@ -1,12 +1,24 @@
-import { FC } from "react";
-import { Character } from "../App/types";
+import { MouseEvent, FC } from "react";
+import { useAppDispatch } from "../App";
+import { Character, TypeAppAction } from "../App/types";
 import { Container } from "./CharacterItemStyles";
 
 export const CharacterItem: FC<Character> = ({ id, name, image }) => {
+  const dispatch = useAppDispatch();
+
+  const deleteHandler = () => {
+    dispatch({ type: TypeAppAction.deleteCharacter, id });
+  };
+
+  const addMemberHandler = (e: MouseEvent<HTMLAnchorElement>) => {
+    e.preventDefault();
+    console.log(name);
+  };
+
   return (
-    <Container href={name}>
+    <Container onClick={addMemberHandler} href={name}>
       <img className="img" src={image} alt={name} />
-      <button className="btn" data-id={id}></button>
+      <button className="btn" onClick={deleteHandler}></button>
     </Container>
   );
 };
