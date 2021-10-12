eb4f868a0a9fd5ec6f223729dbbe30737b603fa4
Vasily Guzov <vasily.guzov@gmail.com>
2021-10-12 14:55:39 +0300

    handleChange for search input

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,6 +1,23 @@
-import { FC } from "react";
+import { ChangeEvent, FC, useEffect, useState } from "react";
 import { SearchInput } from "./SearchStyles";
 
 export const Search: FC = () => {
-  return <SearchInput placeholder="Input Name Character" />;
+  const [inputText, setInputText] = useState("");
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { value },
+    } = e;
+    setInputText(value);
+  };
+
+  useEffect(() => {
+    if (inputText.length > 2) {
+      console.log(inputText);
+    }
+  }, [inputText]);
+
+  return (
+    <SearchInput onChange={handleChange} placeholder="Input Name Character" />
+  );
 };
