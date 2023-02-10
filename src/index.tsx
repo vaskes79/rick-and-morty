44cfaa4d50083e4cd238a8c8c85c79388a158a23
Vasily Guzov <vasily.guzov@gmail.com>
2023-02-10 20:01:35 +0300

    [setup] activate dev tools in prod

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 const client = new ApolloClient({
   uri: process.env.REACT_APP_API_URL,
   cache: new InMemoryCache(),
+  connectToDevTools: true
 });
 
 ReactDOM.render(
